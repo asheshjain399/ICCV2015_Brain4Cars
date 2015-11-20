e24427fe58667b515094787eec7a702a884fddf3
ashesh <ashesh>
2015-11-19 23:44:05 -0800

    adding IOHMM

---
+++ train/IOhmmFit.m
@@ -0,0 +1,328 @@
+%% Implementation of Input-Output HMM
+% Author: Ashesh Jain
+% Email: ashesh@cs.cornell.edu
+
+function [model,ll] = IOhmmFit( truemodel, data, inputObs, model)
+    numiter = 30;
+    ll = zeros(numiter,1);
+    for i = 1:numiter
+        evidence = iocalculateEvidence(model, data, inputObs);
+        [alph,bet,gam,xi,loglikelihood] = FBWpass(model, evidence, inputObs);
+        ll(i) = sum(cell2mat(loglikelihood));
+        sufficient_statistics = estep(gam,model,data,inputObs);
+        model = mstep(gam,xi,sufficient_statistics,model,inputObs,data);
+    end;
+    figure, plot(ll);
+    disp(sum(cell2mat(loglikelihood)));
+    %[~,loglikelihood,~] = ForwardPass(truemodel, calculateEvidence(truemodel, data));
+    %disp(sum(cell2mat(loglikelihood)));
+end
+
+function model = mstep(gam,xi,expected,model,inputObs,data_original)
+    N = size(gam,2);
+  
+    data.inputObs = inputObs;
+    data.nstates = model.nstates;
+    data.inputDimension = model.inputDimension;
+    data.xi = xi;
+    data.gam = gam;
+    
+    funcObj = @objective0;
+    funcGrad = @gradient0;
+    
+    [~,parameters] = minimizeFunc(funcObj,funcGrad,reshape(model.piW',model.inputDimension*(model.nstates-1),1),data);
+    model.piW = reshape(parameters,model.inputDimension,model.nstates-1)';
+    
+    funcObj = @objective;
+    funcGrad = @gradient;
+    
+    nstates = model.nstates;
+    inputDimension = model.inputDimension;
+    Wt = cell(1,nstates);
+    data_complete = cell(1,nstates);
+    for i = 1:model.nstates
+        Wt{i} = model.W(i,:,:);
+        data.state = i;
+        data_complete{i} = data;
+    end;
+    
+    parfor i = 1:model.nstates
+        [~,parameters] = minimizeFunc(funcObj,funcGrad,reshape(reshape(Wt{i},nstates-1,inputDimension)',inputDimension*(nstates-1),1),data_complete{i});
+        Wt{i} = reshape(parameters,inputDimension,nstates-1)';
+    end;
+
+    for i = 1:model.nstates
+        model.W(i,:,:) = Wt{i};
+    end;
+
+    if strcmp(model.type ,'discrete')
+        model.B = expected.observation_count./repmat(expected.state_frequency,1,model.ostates);
+    elseif strcmp(model.type ,'gauss')
+        for i = 1:model.nstates
+            mu = expected.mean_vector(:,i)/expected.state_frequency_const(i);
+            
+            xmu = zeros(model.observationDimension,model.observationDimension);
+            for x = 1:N
+                if model.learn_aparam
+                    model.a = model.aparam{i};
+                else
+                    model.a = [0.5;-0.5;1.0;0];
+                end;
+                lgamma = gam{x};
+                mult_const = (1.0 + sum(repmat(model.a,1,size(lgamma,2)).*inputObs{x},1))';
+                
+                gamma_times_data = repmat(lgamma(i,:),model.observationDimension,1).*data_original{x};
+                xmu = xmu + gamma_times_data*(repmat(mult_const,1,model.observationDimension).*repmat(mu',size(lgamma,2),1));
+            end;
+            
+            %sigma = (expected.cov_matrix(:,:,i) - expected.state_frequency(i)*mu*mu')/expected.state_frequency(i);
+            sigma = (expected.cov_matrix(:,:,i) + expected.state_frequency_const(i)*mu*mu' - xmu - xmu')/expected.state_frequency(i);
+
+            if model.prior.use == 1
+                c1 = model.prior.k0*expected.state_frequency(i)/(model.prior.k0 + expected.state_frequency(i));
+                sigma = model.prior.Psi + sigma + c1*(model.prior.mu0-mu)*(model.prior.mu0-mu)';
+                mu = (model.prior.k0*model.prior.mu0 + expected.state_frequency(i)*mu)/(model.prior.k0+expected.state_frequency(i));
+            end;
+            model.mu{i} = mu;
+            model.sigma{i} = 0.5*(sigma + sigma');
+            sig_inv_mu = (model.sigma{i})\model.mu{i};
+            mu_sig_inv_mu = mu'*sig_inv_mu;
+            if model.learn_aparam
+                model.aparam{i} = (expected.cov_matrix_input(:,:,i))\(expected.cov_matrix_cross(:,:,i)*sig_inv_mu*(1.0/mu_sig_inv_mu) - expected.mean_vector_input(:,i));
+            end;
+        end;
+    end;
+end
+
+function expected = estep(gam,model,data,inputObs)
+    N = size(gam,2);
+    expected.state_frequency = zeros(model.nstates,1);
+    expected.state_frequency_const = zeros(model.nstates,1);
+    for i = 1:N
+        lgamma = gam{i};
+        T = size(lgamma,2);
+        
+        mult_const = zeros(model.nstates,T);
+        for j = 1:model.nstates
+                if model.learn_aparam
+                    model.a = model.aparam{j};
+                else
+                    model.a = [0.5;-0.5;1.0;0];
+                end;
+            mult_const(j,:) = (1.0 + sum(repmat(model.a,1,T).*inputObs{i},1));
+        end;    
+        %mult_const = (1.0 + sum(repmat(model.a,1,T).*inputObs{i},1));
+        mult_const = mult_const.^2;
+        %expected.state_frequency_const = expected.state_frequency_const + sum(lgamma.*repmat(mult_const,model.nstates,1),2);
+        expected.state_frequency_const = expected.state_frequency_const + sum(lgamma.*mult_const,2);
+        expected.state_frequency = expected.state_frequency + sum(lgamma,2);
+    end;
+    
+    if strcmp(model.type,'gauss')
+        expected.mean_vector = zeros(model.observationDimension,model.nstates);
+        expected.mean_vector_input = zeros(model.inputDimension,model.nstates);
+        expected.cov_matrix = zeros(model.observationDimension,model.observationDimension,model.nstates);
+        expected.cov_matrix_input = zeros(model.inputDimension,model.inputDimension,model.nstates);
+        expected.cov_matrix_cross = zeros(model.inputDimension,model.observationDimension,model.nstates);
+        for i = 1:N
+            lgamma = gam{i};
+            T = size(lgamma,2);
+            
+            for j = 1:model.nstates
+                if model.learn_aparam
+                    model.a = model.aparam{j};
+                else
+                    model.a = [0.5;-0.5;1.0;0];
+                end;
+                mult_const = (1.0 + sum(repmat(model.a,1,T).*inputObs{i},1));
+                gamma_times_data_const = repmat(mult_const,model.observationDimension,1).*repmat(lgamma(j,:),model.observationDimension,1).*data{i};
+                gamma_times_data = repmat(lgamma(j,:),model.observationDimension,1).*data{i};
+                gamma_times_input_data = repmat(lgamma(j,:),model.inputDimension,1).*inputObs{i};
+                
+                expected.mean_vector(:,j) = expected.mean_vector(:,j) + sum(gamma_times_data_const,2);
+                expected.mean_vector_input(:,j) = expected.mean_vector_input(:,j) + sum(gamma_times_input_data,2);
+                expected.cov_matrix(:,:,j) = expected.cov_matrix(:,:,j) + gamma_times_data*data{i}';
+                expected.cov_matrix_input(:,:,j) = expected.cov_matrix_input(:,:,j) + gamma_times_input_data*inputObs{i}';
+                expected.cov_matrix_cross(:,:,j) = expected.cov_matrix_cross(:,:,j) + gamma_times_input_data*data{i}';
+            end;
+        end;
+    elseif strcmp(model.type,'discrete')
+        expected.observation_count = zeros(model.nstates,model.ostates);
+        for i = 1:N
+            lgamma = gam{i};
+            for j = 1:model.ostates
+                expected.observation_count(:,j) = expected.observation_count(:,j) + sum(lgamma(:,find(data{i}==j)),2);
+            end;
+        end;
+    end;
+end
+
+function [alph,bet,gam,xi,loglikelihood] = FBWpass(model, evidence, inputObs)
+    %% Here we do the forward-backward pass
+    [alph,loglikelihood,mult_const] = ForwardPass(model, evidence, inputObs);
+    bet = BackwardPass(model, evidence,mult_const,inputObs);
+    gam = cell(1,size(evidence,2));
+    xi = cell(1,size(evidence,2));
+    for i = 1:size(evidence,2)
+        lgam = alph{i}.*bet{i};
+        lgam = lgam./repmat(mult_const{i},model.nstates,1);
+        gam{i} = lgam;
+    end;
+    
+    for i = 1:size(evidence,2)
+        levidence = evidence{i};
+        linputObs = inputObs{i};
+        xi_sample = zeros(model.nstates,model.nstates,size(levidence,2)-1);
+        for j = 1:size(levidence,2)-1
+            A = transitionMatrix(model.W,linputObs(:,j+1),model); 
+            xi_sample(:,:,j) = A.*(alph{i}(:,j)*(levidence(:,j+1).*bet{i}(:,j+1))');
+            val = sum(sum(xi_sample(:,:,j)));
+            
+            %assert(abs(val-1.0)<1e-6);
+        end;
+        xi{i} = xi_sample;
+    end;
+end
+
+function [alph,loglikelihood,mult_const] = ForwardPass(model, evidence, inputObs)
+    %% Here we do the forward pass
+    alph = cell(1,size(evidence,2));
+    loglikelihood = cell(1,size(evidence,2));    
+    mult_const = cell(1,size(evidence,2));    
+    for i = 1:size(evidence,2)
+        levidence = evidence{i};
+        linputObs = inputObs{i};
+        alpha_sample = zeros(model.nstates,size(levidence,2));
+        const = zeros(1,size(levidence,2));
+        starting_prob = multiClassProbability(model.piW,linputObs(:,1));
+        [alpha_sample(:,1), Z] = normalize_local(levidence(:,1).*starting_prob);
+        Zlog = log(Z);
+        const(1) = 1.0/Z;
+        for j = 2:size(levidence,2)
+            A = transitionMatrix(model.W,linputObs(:,j),model); 
+            [alpha_sample(:,j), Z] = normalize_local(levidence(:,j).*(A'*alpha_sample(:,j-1)));
+            Zlog = Zlog + log(Z);
+            const(j) = 1.0/Z;
+        end;
+        alph{i} = alpha_sample;
+        loglikelihood{i} = Zlog;
+        mult_const{i} = const;
+    end;
+end
+
+function bet = BackwardPass(model, evidence, mult_const, inputObs)
+    %% Here we do the backward pass to calculate beta
+    bet = cell(1,size(evidence,2));
+    for i = 1:size(evidence,2)
+        levidence = evidence{i};
+        linputObs = inputObs{i};
+        const = mult_const{i};
+        beta_sample = zeros(model.nstates,size(levidence,2));
+        beta_sample(:,end) = 1;
+        beta_sample(:,end) = const(end)*beta_sample(:,end);
+        for j = (size(levidence,2)-1):-1:1
+            A = transitionMatrix(model.W,linputObs(:,j+1),model); 
+            beta_sample(:,j) = A*(levidence(:,j+1).*beta_sample(:,j+1));
+            beta_sample(:,j) = const(j)*beta_sample(:,j);
+        end;
+        bet{i} = beta_sample;
+    end;
+end
+
+function probability = multiClassProbability(W,U)
+    %% Returns probability of each class Softmax regression
+    potentials = [exp(W*U);1.0];
+    Z = sum(potentials);
+    probability = potentials./Z;
+end
+
+function A = transitionMatrix(W,U,model)
+    %% Returns the state transition matrix for the given input U
+    A = zeros(model.nstates,model.nstates);
+    for i = 1:model.nstates
+        probability = multiClassProbability(reshape(W(i,:,:),model.nstates-1,model.inputDimension),U);
+        A(i,:) = probability';
+    end;
+end
+
+function [v,Z] = normalize_local(v)
+    %% Normalize the vector v
+    Z = sum(v);
+    v = v/Z;
+end
+
+function grad = gradient(w,data)
+    %% Find gradient of j->i transition
+    grad = zeros(size(w,1),1);
+    inputObs = data.inputObs;
+    xi = data.xi;
+    gam = data.gam;
+    state = data.state;
+    nstates = data.nstates;
+    inputDimension = data.inputDimension;
+    for i = 1:size(inputObs,2)
+        linputObs = inputObs{i};
+        lxi = xi{i};
+        lgamma = gam{i};
+        for j = 1:size(linputObs,2)-1
+            probability = multiClassProbability(reshape(w,inputDimension,nstates-1)',linputObs(:,j+1));
+            lxi_ = lxi(:,:,j);
+            assert(sum(lxi_(state,1:end)) - lgamma(state,j) < 1e-8);
+            grad = grad + kron(lxi_(state,1:end-1)' - lgamma(state,j)*probability(1:end-1),linputObs(:,j+1));
+        end;
+    end;
+    grad = -1.0*grad;
+end
+
+function obj = objective(w,data)
+    %% Find objective function value of j->i transition
+    obj = 0.0;
+    inputObs = data.inputObs;
+    xi = data.xi;
+    state = data.state;
+    nstates = data.nstates;
+    inputDimension = data.inputDimension;
+    for i = 1:size(inputObs,2)
+        linputObs = inputObs{i};
+        lxi = xi{i};
+        for j = 1:size(linputObs,2)-1
+            probability = multiClassProbability(reshape(w,inputDimension,nstates-1)',linputObs(:,j+1));
+            lxi_ = lxi(:,:,j);
+            obj = obj + sum(lxi_(state,:)'.*log(probability));
+        end;
+    end;
+    obj = -1.0*obj;
+end
+
+function grad = gradient0(w,data)
+    %% Find gradient of start state of IOHMM
+    grad = zeros(size(w,1),1);
+    inputObs = data.inputObs;
+    gam = data.gam;
+    nstates = data.nstates;
+    inputDimension = data.inputDimension;
+    for i = 1:size(inputObs,2)
+        linputObs = inputObs{i};
+        lgamma = gam{i};
+        probability = multiClassProbability(reshape(w,inputDimension,nstates-1)',linputObs(:,1));
+        grad = grad + kron(lgamma(1:end-1,1) - probability(1:end-1),linputObs(:,1));
+        
+    end;
+    grad = -1.0*grad;
+end
+
+function obj = objective0(w,data)
+    %% Find objective function value of start state of IOHMM
+    obj = 0.0;
+    inputObs = data.inputObs;
+    gam = data.gam;
+    nstates = data.nstates;
+    inputDimension = data.inputDimension;
+    for i = 1:size(inputObs,2)
+        linputObs = inputObs{i};
+        lgamma = gam{i};
+        probability = multiClassProbability(reshape(w,inputDimension,nstates-1)',linputObs(:,1));
+        obj = obj + sum(lgamma(:,1).*log(probability));
+    end;
+    obj = -1.0*obj;
+end
---
+++ train/IOhmmTrain.m
@@ -0,0 +1,321 @@
+function IOhmmTrain()
+    %% Entry point for training HMM
+    clc;
+    addpath(genpath('../Utils'));
+    
+    train_model_for = 'turns';
+    use_fixed_split = false;
+    
+    learnedModels = containers.Map;
+    testData = containers.Map;
+    trainData = containers.Map;
+    testObs = containers.Map;
+    trainObs = containers.Map;
+
+    separate_actions = true;
+    retrain_models = true;
+    delta_frames = 20;
+
+    if separate_actions
+%        actions = {'lchange' ,'rchange','end_action'};
+%        showName = {'L lane','R Lane','Straight'};
+        if strcmp(train_model_for,'turns')
+            actions = {'lturn' ,'rturn','end_action'};
+            showName = {'L Turn','R Turn','Straight'};
+            fixed_split_name = 'fold_turns_';
+            thresh = 60;
+        elseif strcmp(train_model_for,'lane')
+            actions = {'lchange','rchange','end_action'};
+            showName = {'L Lane','R Lane','Straight'};
+            fixed_split_name = 'fold_io2_';
+            thresh = 130;
+        elseif strcmp(train_model_for,'all')
+            actions = {'lchange' ,'rchange','lturn' ,'rturn','end_action'};
+            showName = {'L Lane','R Lane','L Turn','R Turn','Straight'};
+            thresh = 130;
+            fixed_split_name = '';
+        else
+            actions = {'lchange' ,'rchange','end_action'};
+            showName = {'L Lane','R Lane','Straight'};
+            train_model_for = 'turns';
+            fixed_split_name = 'fold_io2_';
+            thresh = 130;
+        end;
+    else
+        train_model_for = 'combined';
+        actions = {'combined','end_action'};
+    end;
+
+    fname_ext = '_f_13_ww_20_df_20.mat';
+    disp(fname_ext);
+    folds_action = containers.Map;
+    for i = 1:size(actions,2)
+        action = actions{i};
+        load(['features/' action fname_ext]);      
+        size_fold = round(min(size(data,2),thresh)/5.0);
+        rand_list = randperm(min(size(data,2),thresh));
+        folds={};
+        folds{1} = rand_list(1:1:size_fold);
+        folds{2} = rand_list((size_fold+1):1:(2*size_fold));
+        folds{3} = rand_list((2*size_fold+1):1:(3*size_fold));
+        folds{4} = rand_list((3*size_fold+1):1:(4*size_fold));
+        folds{5} = rand_list((4*size_fold+1):1:(min(thresh,size(data,2))));
+        folds_action(action) = folds;
+    end;
+    
+    if retrain_models
+        for numiter = 1:5
+            if use_fixed_split
+                if exist(['features/' fixed_split_name num2str(numiter) '.mat'],'file') == 2
+                    load(['features/' fixed_split_name num2str(numiter) '.mat']);
+                else
+                    use_fixed_split = false;
+                end;
+            end;
+            learnedModels_ = {};
+            testData_ = {};
+            trainData_ = {};
+            testObs_ = {};
+            trainObs_ = {};
+            parfor i = 1:size(actions,2)
+                action = actions{i};
+
+                if use_fixed_split
+                    test_data = testData(action);
+                    train_data = trainData(action);
+                    test_obs = testObs(action);
+                    train_obs = trainObs(action);
+                else
+                    folds = folds_action(action);
+                    load_data = load(['features/' action fname_ext]);
+                    data = load_data.data;
+                    inputObs = load_data.inputObs;
+                    test_data = data(folds{i});
+                    test_obs = inputObs(folds{i});
+                    train_idx = 1:min(thresh,size(data,2));
+                    train_idx(folds{i}) = [];
+                    train_data = data(train_idx);
+                    train_obs = inputObs(train_idx);
+                end;
+
+                model.type = 'gauss';
+                model.iotype = true;
+                model.observationDimension = size(train_data{1},1);
+                model.inputDimension = size(train_obs{1},1);
+                if strcmp(action,'end_action')
+                    model.nstates=3;
+                elseif strcmp(action,'rchange')
+                    model.nstates=6;
+                elseif strcmp(action,'lchange')
+                    model.nstates=5;
+                elseif strcmp(action,'rturn')
+                    model.nstates=3;
+                elseif strcmp(action,'lturn')
+                    model.nstates=3;
+                elseif strcmp(action,'combined')
+                    model.nstates=6;
+                end;
+
+                model = initializeHMMmodel(model.type,model.nstates,model.observationDimension,model.iotype,model.inputDimension);
+                %model.a = [0.5;-0.5;1.0;0];
+                model.a = [0;0;0;0];
+                model.action = action;
+                model.learn_aparam = false;
+                
+
+                model.prior.use = 1; % 1 use the prior, 0 not prior on parameters
+                %% Prior values
+                model.prior.k0 = 1;
+                model.prior.mu0 = (1.0/model.observationDimension)*ones(model.observationDimension,1);
+                model.prior.Psi = (1.0/model.observationDimension)*eye(model.observationDimension,model.observationDimension);
+
+
+                [model,ll] = IOhmmFit(model,train_data,train_obs,model);
+                learnedModels_{i} = model;
+                testData_{i} = test_data;
+                trainData_{i} = train_data;
+                testObs_{i} = test_obs;
+                trainObs_{i} = train_obs;
+            end;
+            for i = 1:size(actions,2)
+                action = actions{i};
+                learnedModels(action) = learnedModels_{i};
+                testData(action) = testData_{i};
+                trainData(action) = trainData_{i};                
+                testObs(action) = testObs_{i};
+                trainObs(action) = trainObs_{i};                
+            end;
+            save(['features/learnedModel_' train_model_for '_iohmm_NoA_' num2str(numiter) fname_ext],'learnedModels','testData','trainData','testObs',...
+                'trainObs','folds_action');
+            %{
+            if separate_actions
+                save(['observations/learnedModel_alldata_io_' num2str(numiter) fname_ext '.mat'],'learnedModels','testData','trainData','testObs','trainObs');
+            else
+                save(['observations/learnedModel_alldata_io_combined_' num2str(numiter) fname_ext '.mat'],'learnedModels','testData','trainData','testObs','trainObs');
+            end;
+            %}
+        end;
+    end;
+    p=[];
+    r=[];
+    p_left = [];
+    p_right = [];
+    r_left = [];
+    r_right = [];
+    p_with_std = [];
+    r_with_std = [];
+    p_with_mean = [];
+    r_with_mean = [];
+    
+    for THRESH = 0.0:0.1:1.0
+        time_global = [];
+       true_label = [];
+    predicted_label = [];
+    accuracy = []; 
+        for numiter = 1:5
+            load(['features/learnedModel_' train_model_for '_iohmm_NoA_' num2str(numiter) fname_ext]);
+            %{
+            if separate_actions
+                load(['observations/learnedModel_io3_' num2str(numiter) fname_ext '.mat']);
+            else
+                load(['observations/learnedModel_io3_combined_' num2str(numiter) fname_ext '.mat']);
+            end;
+            %}
+            prediction_local = [];
+            true_label_local = [];
+
+            for i = 1:size(actions,2)
+                action = actions{i};
+                test_data = testData(action);
+                test_obs = testObs(action);
+                for j = 1:size(test_data,2)
+                    true_label = [true_label,i];
+                    true_label_local = [true_label_local,i];
+                    prediction_probability = timeSeriesPrediction(learnedModels,test_data{j},test_obs{j},actions);      
+                    [action_predict,time_before] = predictAction(prediction_probability,actions,delta_frames,THRESH);
+                    predicted_label = [predicted_label action_predict];
+                    prediction_local = [prediction_local action_predict];
+                    time_global = [time_global time_before];
+                end;
+            end;
+            confMat_local = confusionMatrix(prediction_local,true_label_local);
+            p_lchange_local = confMat_local(1,1)*100.0/sum(confMat_local(:,1));
+            p_rchange_local = confMat_local(2,2)*100.0/sum(confMat_local(:,2));
+            r_lchange_local = confMat_local(1,1)*100.0/sum(confMat_local(1,:));
+            r_rchange_local = confMat_local(2,2)*100.0/sum(confMat_local(2,:));
+            p_iter(numiter) = mean([p_lchange_local p_rchange_local]);        
+            r_iter(numiter) = mean([r_lchange_local r_rchange_local]);        
+            disp([num2str(numel(find(prediction_local == true_label_local))) '/' num2str(size(true_label_local,2))]);
+            accuracy = [accuracy numel(find(prediction_local == true_label_local))*100.0/size(true_label_local,2)];
+        end;
+        
+        disp(['THRESH= ' num2str(THRESH)]);
+        matching = find(predicted_label == true_label);
+        time_global = time_global(matching);
+        time_global(find(time_global == -1)) = [];
+        disp(['Mean time = ' num2str(mean(time_global))]);
+        confMat = confusionMatrix(predicted_label,true_label);
+        %p = [p (confMat(1,1) + confMat(2,2))/sum(sum(confMat(:,1:2)))];
+        %r = [r (confMat(1,1) + confMat(2,2))/sum(sum(confMat(1:2,:)))];
+        pr = (confMat(1,1)*1.0/sum(confMat(:,1)));% + confMat(2,2)*1.0/sum(confMat(:,2)));
+        re = (confMat(1,1)*1.0/sum(confMat(1,:))); % + confMat(2,2)*1.0/sum(confMat(2,:)));
+        disp(['accuracy = ' num2str(mean(accuracy)) '(' num2str(std(accuracy)) ')']);
+        
+        p_lchange = confMat(1,1)*100.0/sum(confMat(:,1));
+        p_rchange = confMat(2,2)*100.0/sum(confMat(:,2));
+        p_end_act = confMat(3,3)*100.0/sum(confMat(:,3));
+        disp(['precision lchange = ' num2str(p_lchange)]);
+        disp(['precision rchange = ' num2str(p_rchange)]);
+        disp(['precision end_act = ' num2str(p_end_act)]);
+        
+        r_lchange = confMat(1,1)*100.0/sum(confMat(1,:));
+        r_rchange = confMat(2,2)*100.0/sum(confMat(2,:));
+        r_end_act = confMat(3,3)*100.0/sum(confMat(3,:));
+        disp(['recall lchange = ' num2str(r_lchange)]);
+        disp(['recall rchange = ' num2str(r_rchange)]);
+        disp(['recall end_act = ' num2str(r_end_act)]);
+        
+        disp(confMat./repmat(sum(confMat,2),1,size(confMat,2)));
+        savename = ['Figures/confMat_iohmm_' fname_ext '_' num2str(THRESH) '.jpg'];
+        %visualizeConfMat( confMat./repmat(sum(confMat,2),1,size(confMat,2)),showName,savename );
+        p = [p mean([p_lchange p_rchange])];
+        r = [r mean([r_lchange r_rchange])];
+        p_with_std = [p_with_std std(p_iter)];
+        r_with_std = [r_with_std std(r_iter)];
+        p_with_mean = [p_with_mean mean(p_iter)];
+        r_with_mean = [r_with_mean mean(r_iter)];
+
+    end;
+    p = 0.01*p;
+    r = 0.01*r;
+    disp(p);
+    disp(r);
+    disp('Mean')
+    disp(0.01*p_with_mean);
+    disp(0.01*r_with_mean);
+    disp('std')
+    disp(0.01*p_with_std);
+    disp(0.01*r_with_std);
+
+    plot(p,r);
+    xlim([0 1]);
+    ylim([0 1]);
+    
+    
+end
+
+function [action_taken,time_before] = predictAction(probability_table,actions,delta_frames,THRESH)
+    %THRESH = 0.7;
+    time_before = -1;
+    T = size(probability_table,1);
+    [~,action_taken] = ismember('end_action',actions);
+    for i = 1:T
+        [val,idx] =  max(probability_table(i,:));
+        if val > THRESH && idx ~= action_taken && probability_table(i,action_taken) < THRESH
+            action_taken = idx;
+            time_before = (T-i)*delta_frames*1.0/25.0;
+            return
+        end;
+    end;
+end
+
+function prediction_probability = timeSeriesPrediction(learnedModels,test_data,test_obs,actions)
+    T = size(test_data,2);
+    prediction_probability = zeros(T,size(actions,2));
+    for j = 1:T 
+        test = {};
+        test{1} = test_data(:,1:j);
+        obs{1} = test_obs(:,1:j);
+        loglikelihood = zeros(1,size(actions,2));
+        for i = 1:size(actions,2)
+            action = actions{i};
+            model = learnedModels(action);  
+            model.save_and_print = false;
+            ll = iomodelDataLoglikelihood(model,iocalculateEvidence(model,test,obs),obs);
+            loglikelihood(i) = ll{1}; 
+        end;
+        log_a_b = logTologOfSum(loglikelihood);
+        prediction_probability(j,:) = exp(loglikelihood - log_a_b);
+    end;
+end
+
+function log_a_b = logTologOfSum(loglikelihood)
+    if size(loglikelihood,2) == 2
+        v1 = loglikelihood(1);
+        v2 = loglikelihood(2);
+        log_a_b = logOfsum(v1,v2);
+    else
+        v1 = loglikelihood(1);
+        v2 = loglikelihood(1,2:end);
+        log_a_b = logOfsum(v1,logTologOfSum(v2));
+    end;
+end
+
+function confMat = confusionMatrix(predict,actual)
+    lchange = find(actual==1);
+    rchange = find(actual==2);
+    nchange = find(actual==3);
+    confMat = [numel(find(predict(lchange)==1)),numel(find(predict(lchange)==2)),numel(find(predict(lchange)==3));...
+        numel(find(predict(rchange)==1)),numel(find(predict(rchange)==2)),numel(find(predict(rchange)==3));...
+        numel(find(predict(nchange)==1)),numel(find(predict(nchange)==2)),numel(find(predict(nchange)==3))];
+end
